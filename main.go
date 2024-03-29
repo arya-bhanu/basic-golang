d4b5cbd75ca44cf4a7fda517887104c4446a756f
arya-bhanu <aryacreative.dev@gmail.com>
2024-03-29 09:53:31 +0700

    ini variable branch

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// declaration with variable type [manifest typing]
+	var firstName string = "Putu"
+	// this will result warning, go compiler is smart enough to tell declaration and initialization value must on the same line
+	var lastName string
+	lastName = "Bhanuartha"
+	fmt.Println(firstName + lastName)
+
+	// declaration without variable type [type inference]
+	// --deklarasi ini hanya bisa dilakuka dalam blok fungsi, tidak dalam scope global
+	firstName1 := "Putu"
+	lastName1 := "Bhanuartha"
+	//  print using Printf
+	fmt.Printf("Hallo %s %s !, selamat datang\n", firstName1, lastName1)
+
+	// multiple declaration variable
 }
