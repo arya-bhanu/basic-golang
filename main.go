e02cc3ecace44e3b59a8af933ad7a90ad624fee8
arya-bhanu <aryacreative.dev@gmail.com>
2024-03-29 14:38:56 +0700

    feat: final variable

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	// declaration with variable type [manifest typing]
+	// Declaration With Variable Type [manifest typing]
 	var firstName string = "Putu"
-	// this will result warning, go compiler is smart enough to tell declaration and initialization value must on the same line
+	// --this will result warning, go compiler is smart enough to tell declaration and initialization value must on the same line
 	var lastName string
 	lastName = "Bhanuartha"
 	fmt.Println(firstName + lastName)
 
-	// declaration without variable type [type inference]
-	// --deklarasi ini hanya bisa dilakuka dalam blok fungsi, tidak dalam scope global
+	// Declaration Without Variable Type [type inference]
+	// --declaration only on function scope, not in global scope
 	firstName1 := "Putu"
 	lastName1 := "Bhanuartha"
 	//  print using Printf
 	fmt.Printf("Hallo %s %s !, selamat datang\n", firstName1, lastName1)
 
-	// multiple declaration variable
+	// Multiple Declaration Variable
+	// --using [manifest typing]
+	var first, second, third = 12, 23, 12
+	fmt.Println(first + second + third)
+	// --using [type inference]
+	seventh, eight, nine := 12, "8", 9.0
+	fmt.Printf("Seventh : %s, eight : %s , nine : %s ", strconv.Itoa(seventh), eight, fmt.Sprintf("%f", nine))
+
+	// Reserved Variable
+	// -- just like in javascript, it will ignore unused variable, but in golang, it will throw it away
+	var first1, _ = first, second
+	fmt.Println(first1)
+
+	// Declare Variable Using "new" Keyword
+	stringMem := new(string)
+	// --return it's memory address
+	fmt.Println("Memory address : " + fmt.Sprintf("%p", stringMem))
+	// --dereference
+	fmt.Println("Dereference memory : " + *stringMem)
+
+	// Declare Variable Using Keyword "make"
 }
