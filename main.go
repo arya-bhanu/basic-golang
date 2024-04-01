5db9e3ea314de5f24e5e2e9a20e252d4819ff028
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-01 10:53:33 +0700

    feat: constant

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+)
 
 func main() {
-	fmt.Println("Hello World")
+	const phi = math.Phi
+	// phi = 12000 --> results error
+	fmt.Println(phi)
+
+	// multiple constant declaration
+	const (
+		square         = "kotak"
+		isToday  bool  = true
+		numeric  uint8 = 1
+		floatNum       = 2.2
+	)
+	fmt.Printf("Square : %s\nisToday : %t\nnumeric : %d\nfloatNum : %f\n", square, isToday, numeric, floatNum)
+
+	// multiple constant declaration with inherit
+	const (
+		a = "1"
+		b
+		c
+	)
+
+	fmt.Println(a, b, c)
+
+	// one line multiple constant declaration
+	const satu, dua = 1, 2
+	fmt.Println(satu + dua)
 }
