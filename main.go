d32c1c7318a6a09c65738ada2f36150bba904303
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-02 21:48:26 +0700

    feat: perulangan

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// perulangan di golang hanya for, tidak ada while, foreach, namun bisa membentuk cara kerja yang sama
+	// standar perulangan
+	for i := 0; i < 10; i++ {
+		fmt.Println("Standar looping : ", i)
+	}
+
+	// "while" like looping
+	angka := 1
+	for angka < 5 {
+		fmt.Println("While like looping ", angka)
+		angka++
+	}
+
+	// for without any argument
+	for {
+		fmt.Println("For loop without argument : ", angka)
+		if angka == 0 {
+			break
+		}
+		angka--
+	}
+
+	// for looping with range
+	const hello = "hello"
+	for i, j := range hello {
+		fmt.Printf("Indeks : %d, Character : %s\n", i, strconv.QuoteRune(j))
+	}
+
+	// for looping with label
+outerLoop:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i == 3 {
+				break outerLoop
+			}
+			fmt.Print("matriks [", i, "][", j, "]", "\n")
+		}
+	}
+
 }
