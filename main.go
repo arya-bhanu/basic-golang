698c72fcfdf8151cca40f3f4862c92e90b66b108
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-12 11:53:54 +0800

    feat: pointer

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var memA *int
+	var numberA = 4
+	var numberB = 4
+
+	memA = &numberA
+	*memA = 20
+
+	fmt.Println(&numberA)
+	fmt.Println(&numberB)
+	fmt.Println(*memA)
+
+	numberA = 10
+	fmt.Println(*memA)
+
+	// change value with function
+	var number = 4
+	fmt.Println(number)
+	fmt.Println(&number)
+	change(&number, 10)
+	fmt.Println(number)
+	fmt.Println(&number)
+}
+
+func change(address *int, number int) {
+	*address = number
 }
