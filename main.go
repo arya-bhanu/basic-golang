dd447ac3cdba240b62110e2e6b08a5199c5d1c16
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-13 15:58:16 +0800

    feat: reflect

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var number = 23
+	var reflectValueNumber = reflect.ValueOf(number)
+	if reflectValueNumber.Kind() == reflect.Int {
+		fmt.Println("tipe variable: ", reflectValueNumber.Type())
+		fmt.Println("nilai variable: ", reflectValueNumber.Int())
+	}
+
+	
 }
