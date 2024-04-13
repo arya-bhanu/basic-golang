dca154713aedf62bd37e46866b6a5b6df966d08b
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-13 14:52:48 +0800

    feat: interface

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type hitung interface {
+	luas() float64
+	keliling() float64
+}
+
+type lingkaran struct {
+	diameter float64
+}
+
+type persegi struct {
+	sisi float64
+}
+
+func (p *persegi) luas() float64 {
+	return p.sisi * p.sisi
+}
+
+func (p *persegi) keliling() float64 {
+	return 4 * p.sisi
+}
+
+func (l lingkaran) jariJari() float64 {
+	return l.diameter / 2
+}
+
+func (l lingkaran) luas() float64 {
+	return math.Pi * math.Pow(l.jariJari(), 2)
+}
+func (l lingkaran) keliling() float64 {
+	return math.Pi * l.diameter
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var bangunDatarLingkaran hitung = lingkaran{diameter: 20.0}
+	var bangunDatarPersegi hitung = &persegi{sisi: 10.0}
+
+	fmt.Println(bangunDatarLingkaran.keliling())
+	fmt.Println(bangunDatarPersegi.keliling())
 }
