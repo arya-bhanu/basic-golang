bd006c437dff4ccfa08421e49d1f914d49a15dd9
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-28 21:31:52 +0700

    feat: go routine

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
+
+func print(amount int, input string) {
+	for val := range amount {
+		fmt.Println((val + 1), input)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	runtime.GOMAXPROCS(4)
+	go print(5, "Halo")
+	print(5, "Apa kabar?")
+
+	var word string
+	var count int
+	fmt.Scanln(&word)
+	fmt.Scanln(&count)
+	fmt.Println(word)
+	fmt.Println(count)
+	for val := range count {
+		_ = val
+		fmt.Println(word)
+	}
 }
